b6254e133d2f2e44ba053456cf11895aa550f61d
Wesley Fuchter <wesley.fuchter@moduscreate.com>
2022-07-08 13:26:40 -0300

    money-app

--- money-app/pipeline.sh
+++ money-app/pipeline.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd ./balance-service/ && mvn clean install package -DskipTests
+
+cd ..
+
 docker build -f ./transaction-service/Dockerfile -t wesleyfuchter/transaction-service:v1 .
 docker build -f ./balance-service/Dockerfile -t wesleyfuchter/balance-service:v1 .
 
